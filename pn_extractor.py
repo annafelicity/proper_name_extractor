9a42ec358c45a6db5adc0022217d76ce7b5447d1
Anna Felicity Friedman <annafelicityfriedman@gmail.com>
2017-05-24 13:58:31 -0500

    edits for legibility

--- pn_extractor.py
+++ pn_extractor.py
@@ -1,13 +1,18 @@
 #this script is a draft to extract proper names from a string
 #it mainly focuses on personal names, but this will also catch some corporate names too
-#(this can pose a problem if you only want people's names, but you can add corporate stopwords to a stopword list)
+#(this can pose a problem if you only want people's names, 
+#but you can add corporate stopwords to a stopword list)
 
 #note 1: this does not pull them in order, it just pulls them
 
 #note 2: the regexs need to be ordered by least likely to be matched to most likely to be matched, for any new patterns added in
 
 #this will need many more regex patterns added to account for all name-pattern variations
-#I have tried to make the pattern names descriptive of the patterns, i.e. "initialdot_last_suffixdot" corresponds to names like "J. Mascis Jr.", "first_initial_apostrophiclast" to "S. O'Connor", "first_initialdot_last" to "Mary J. Blige", "first_last" to "Kim Gordon", etc. etc.
+#I have tried to make the pattern names descriptive of the patterns, 
+#i.e. "initialdot_last_suffixdot" corresponds to names like "J. Mascis Jr.", 
+#"first_initial_apostrophiclast" to "S. O'Connor", 
+#"first_initialdot_last" to "Mary J. Blige", 
+#"first_last" to "Kim Gordon", etc. etc.
 
 def pn_extractor(string):
     initialdot_last_suffixdot = '([A-Z]\.\s[A-Z][a-z]{1,}\s[A-Z][a-z]\.)'
@@ -28,7 +33,8 @@ def pn_extractor(string):
 #base code for stopwords found here: http://stackoverflow.com/questions/25346058/removing-list-of-words-from-a-string
 
 def remove_stopwords(string):
-    stopwords = ['Musician','Rock','Band'] #this will eliminate "Kid Rock" from being extracted, but I think that's a good thing...
+    stopwords = ['Musician','Rock','Band'] 
+    #this will eliminate "Kid Rock" from being extracted, but I think that's a good thing...
     querywords = string.split()
     resultwords  = [word for word in querywords if word not in stopwords]
     return ' '.join(resultwords)
